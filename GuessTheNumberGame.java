f694d0ccf2752633ccd3951fa3c075ff35e78738
Chirag Mendiratta <chiragmendiratta27@gmail.com>
2025-03-18 13:36:46 +0530

    GuessTheNumberGame.java

--- GuessTheNumberGame.java
+++ GuessTheNumberGame.java
@@ -2,17 +2,15 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class GuessTheNumberGame {
-    private int randomNumber; // The random number to be guessed
-    private int noOfGuesses; // Number of guesses taken by the user
-
-    // Constructor to generate a random number
+    private int randomNumber; 
+    private int noOfGuesses; 
+    
     public GuessTheNumberGame() {
         Random rand = new Random();
-        this.randomNumber = rand.nextInt(100) + 1; // Random number between 1 and 100
-        this.noOfGuesses = 0; // Initialize the number of guesses to 0
+        this.randomNumber = rand.nextInt(100) + 1; 
+        this.noOfGuesses = 0; 
     }
 
-    // Method to take user input
     public int takeUserInput() {
         Scanner S1 = new Scanner(System.in);
         System.out.print("Enter your guess (between 1 and 100): ");
@@ -20,9 +18,8 @@ public class GuessTheNumberGame {
         return userGuess;
     }
 
-    // Method to check if the guessed number is correct
     public boolean isCorrectNumber(int userGuess) {
-        noOfGuesses++; // Increment the number of guesses
+        noOfGuesses++; 
         if (userGuess == randomNumber) {
             System.out.println("Congratulations! You guessed the correct number in " + noOfGuesses + " guesses.");
             return true;
@@ -34,17 +31,10 @@ public class GuessTheNumberGame {
         return false;
     }
 
-    // Getter for noOfGuesses
     public int getNoOfGuesses() {
         return noOfGuesses;
     }
 
-    // Setter for noOfGuesses (optional, not required for this game)
-//    public void setNoOfGuesses(int noOfGuesses) {
-//        this.noOfGuesses = noOfGuesses;
-//    }
-
-    // Main method to play the game
     public static void main(String[] args) {
         GuessTheNumberGame game = new GuessTheNumberGame();
         boolean hasGuessedCorrectly = false;
